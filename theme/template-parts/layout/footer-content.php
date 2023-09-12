5f6f0b6aa5f2c6b795601523a66b4cd1196f156d
Sibylle Eid <sib.weber@gmail.com>
2023-09-12 05:24:44 +0200

    first commit

---
+++ theme/template-parts/layout/footer-content.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Template part for displaying the footer content
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package sibyllesblog_tw
+ */
+
+?>
+
+<footer id="colophon">
+
+	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+		<aside role="complementary" aria-label="<?php esc_attr_e( 'Footer', 'sibyllesblog_tw' ); ?>">
+			<?php dynamic_sidebar( 'sidebar-1' ); ?>
+		</aside>
+	<?php endif; ?>
+
+	<?php if ( has_nav_menu( 'menu-2' ) ) : ?>
+		<nav aria-label="<?php esc_attr_e( 'Footer Menu', 'sibyllesblog_tw' ); ?>">
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-2',
+					'menu_class'     => 'footer-menu',
+					'depth'          => 1,
+				)
+			);
+			?>
+		</nav>
+	<?php endif; ?>
+
+	<div>
+		<?php
+		$sibyllesblog_tw_blog_info = get_bloginfo( 'name' );
+		if ( ! empty( $sibyllesblog_tw_blog_info ) ) :
+			?>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>,
+			<?php
+		endif;
+
+		/* translators: 1: WordPress link, 2: WordPress. */
+		printf(
+			'<a href="%1$s">proudly powered by %2$s</a>.',
+			esc_url( __( 'https://wordpress.org/', 'sibyllesblog_tw' ) ),
+			'WordPress'
+		);
+		?>
+	</div>
+
+</footer><!-- #colophon -->
